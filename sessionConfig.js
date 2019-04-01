e5b0df230f342c88a0c6b65185d0a59d659c7d03
mark-marshall <mark.marshallgp@gmail.com>
2019-04-01 15:42:33 +0100

    [MVP] Changed session store to db

--- sessionConfig.js
+++ sessionConfig.js
@@ -1,3 +1,7 @@
+const session = require('express-session');
+const KnexSessionStore = require('connect-session-knex')(session);
+const db = require('./knexConfig');
+
 const sessionConfig = {
     name: 'hippo',
     secret: 'shh',
@@ -8,6 +12,13 @@ const sessionConfig = {
     httpOnly: true,
     resave: false,
     saveUninitialized: false,
+    store: new KnexSessionStore({
+      knex: db,
+      tablename: 'sessions',
+      sidfieldname: 'sid',
+      createtable: true,
+      clearInterval: 1000 * 60 * 60,
+    }),
   };
 
   module.exports = sessionConfig;
