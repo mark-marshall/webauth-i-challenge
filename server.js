ff1e3c29a39cde8cf6a932b3b41ecb22208cc163
mark-marshall <mark.marshallgp@gmail.com>
2019-04-01 12:51:02 +0100

    [MVP] Created basic register and login endpoints without bcrypt

--- server.js
+++ server.js
@@ -2,13 +2,71 @@ const express = require('express');
 const helmet = require('helmet');
 const knex = require('knex');
 const bcrypt = require('bcryptjs');
-
-// const db = require('./database/dbConfig.js');
-// const Users = require('./users/users-model.js');
+const userDb = require('./handlers/userHandlers');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
+
+/*
+=========== USER REGISTRATION
+[POST] request with the following sent in the body;
+"username": "string up to 128 characters (unique)",
+"password": "string"
+*/
+server.post('/api/register', (req, res) => {
+  const user = req.body;
+
+  if (user.username && user.password) {
+    userDb
+      .add(user)
+      .then(user => {
+        res.status(201).json(user);
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: 'there was an error adding this user to the database',
+        });
+      });
+  } else {
+    res
+      .status(404)
+      .json({ message: 'you must enter a username and password to register' });
+  }
+});
+
+/*
+=========== USER LOGIN
+[POST] request with the following sent in the body;
+"username": "valid username that exists already in the database",
+"password": "valid password that matches the usernames credentials"
+*/
+server.post('/api/login', (req, res) => {
+  const { username, password } = req.body;
+
+  if (username && password) {
+    userDb
+      .findBy({ username })
+      .then(user => {
+        if (user && user.password === password) {
+          res.status(200).json({ message: `welcome in ${username}` });
+        } else {
+          res
+            .status(401)
+            .json({ message: 'the credentials you entered were invalid' });
+        }
+      })
+      .catch(err =>
+        res.status(500).json({ message: 'the user could not be logged in' }),
+      );
+  } else {
+    res
+      .status(404)
+      .json({
+        message: 'please enter both your username and password to login',
+      });
+  }
+});
 
 module.exports = server;
