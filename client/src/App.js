9f0896946b129ac7a5b602dd064a12e2b3dac104
mark-marshall <mark.marshallgp@gmail.com>
2019-04-02 13:49:36 +0100

    [STRETCH] Fleshed out skeleton for register, login, and users components

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,106 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+import Register from './Register';
+import Login from './Login';
+import Users from './Users';
 
 class App extends Component {
+  state = {
+    user: {
+      username: '',
+      password: '',
+    },
+    login: {
+      username: '',
+      password: '',
+    },
+    users: [],
+  };
+
+  componentDidMount() {
+    if (localStorage.getItem('auth')) {
+      axios
+        .get('http://localhost:4000/api/users')
+        .then(users => this.setUsers(users.data))
+        .catch(err => console.log(err));
+    }
+  }
+
+  setUsers = users => {
+    this.setState({
+      users,
+    });
+  };
+
+  resetEntry = () => {
+    this.setState({
+      user: {
+        username: '',
+        password: '',
+      },
+    });
+  };
+
+  resetLogin = () => {
+    this.setState({
+      login: {
+        username: '',
+        password: '',
+      },
+    });
+  };
+
+  onChangeEntry = event => {
+    this.setState({
+      user: {
+        ...this.state.user,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  onChangeLogin = event => {
+    this.setState({
+      login: {
+        ...this.state.login,
+        [event.target.name]: event.target.value,
+      },
+    });
+  };
+
+  registerUser = user => {
+    axios
+      .post('http://localhost:4000/api/register', user)
+      .then(localStorage.setItem('auth', true))
+      .then(this.resetEntry())
+      .catch(err => console.log(err));
+  };
+
+  loginUser = user => {
+    axios
+      .post('http://localhost:4000/api/login', user)
+      .then(localStorage.setItem('auth', true))
+      .then(this.resetLogin())
+      .catch(err => console.log(err));
+  };
+
   render() {
-    return <div className="App">Hello World</div>;
+    return (
+      <div>
+        <Register
+          user={this.state.user}
+          changeHandler={this.onChangeEntry}
+          registerUser={this.registerUser}
+        />
+        <Login
+          user={this.state.login}
+          changeHandler={this.onChangeLogin}
+          loginUser={this.loginUser}
+        />
+        <Users users={this.state.users}/>
+      </div>
+    );
   }
 }
 
